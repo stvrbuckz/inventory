6de057a29ffd1c3bfaac9e9c32d9422262d3054e
up900822 <60429245+up900822@users.noreply.github.com>
2021-06-04 17:59:21 +0100

    product query - select product info

--- backend/api/routes/products/products.queries.js
+++ backend/api/routes/products/products.queries.js
@@ -3,6 +3,7 @@ const Knex = require('knex');
 
 module.exports = {
     find() {
-        return db('product');
+        // filter by...
+        return db('product').select('prod_id', 'prod_name', 'prod_brand', 'prod_price', 'is_available', 'available_quantity', 'location');
     },
 };
