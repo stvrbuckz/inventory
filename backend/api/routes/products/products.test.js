6ec690c47c15f59242a8c4ca37e61a2298f6512d
up900822 <60429245+up900822@users.noreply.github.com>
2021-06-10 18:42:47 +0100

    added frontend pages: orders, products, purchases, staff and suppliers

--- backend/api/routes/products/products.test.js
+++ backend/api/routes/products/products.test.js
@@ -2,12 +2,11 @@ const supertest = require('supertest');
 const app = require('../../../app');
 
 describe('GET /api/routes/products', () => {
-    it('should  respond with an array of products', async () => {
+    it('should respond with an array of products equal to 170', async () => {
         const response = await supertest(app)
             .get('/api/routes/products')
             .expect(200);
 
-        // expect(response.body).toEqual([]);
-        expect(response.body.length).toBeGreaterThan(0);
+        expect(response.body.length).toEqual(170);
     });
 });
